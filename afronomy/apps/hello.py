1a5437a5d8aa566d68a58ded716d9be3c016b341
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2024-11-24 20:32:46 +0200

    Update hello.py

--- afronomy/apps/hello.py
+++ afronomy/apps/hello.py
@@ -1,10 +1,14 @@
 import click
 
+def hello(name: str) -> str:
+    """Function that returns a greeting string."""
+    return f"Hello {name}"
+
 @click.command()
 @click.option('--name', prompt='Please enter your name', help='The name to greet.')
-def hello(name):
-    """Prints 'Hello name' to the terminal from input."""
-    click.echo(f"Hello {name}")
+def main(name):
+    """CLI entry point."""
+    click.echo(hello(name))
 
 if __name__ == '__main__':
-    hello()
+    main()
