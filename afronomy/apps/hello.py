be0620b3bb5e69e9d6896960c8908381bb6f5cb6
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2024-11-24 21:09:23 +0200

    Update hello.py

--- afronomy/apps/hello.py
+++ afronomy/apps/hello.py
@@ -8,14 +8,20 @@ def hello(name: str) -> str:
     """Function that returns a greeting string."""
     return f"Hello {name}"
 
-@click.command()
+@click.group()
 @click.option('--name', prompt='Please enter your name', help='The name to greet.')
 def main(name):
     """CLI entry point."""
     click.echo(hello(name))
 
 schemas = OmegaConf.load(os.path.join(os.path.dirname(__file__), "../cabs/hello.yml"))
-print(schemas)
+
+def clickify(name, schema_name=None):
+    schema_name = schema_name or name
+    return lambda func: \
+        cli.command(name, help=schemas.cabs.get(f'afronomy.{schema_name}').info,no_args_is_help=True)(
+                clickify_parameters(schemas.cabs.get(f'afronomy.{schema_name}'))(func)
+        )
 
 if __name__ == '__main__':
     main()
