4d34c93fb1ba8cb44d1d31225c2b1b499eccd76c
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2024-11-25 14:44:08 +0200

    Write to file

--- afronomy/__init__.py
+++ afronomy/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 import logging
 
-__version__ = '0.0.1'
+__version__ = '0.0.1-rc.1'
 
 
 def logo():
--- afronomy/apps/hello.py
+++ afronomy/apps/hello.py
@@ -5,20 +5,32 @@ import click
 from omegaconf import OmegaConf
 from scabha.schema_utils import clickify_parameters
 
-@click.group()
-def main():
+@click.command()
+@click.argument('name')
+@click.option('-o', '--outfile', default='hello.txt', help='Path to the output text file.')
+def main(name, outfile):
     """CLI entry point."""
-    pass
+    click.echo(hello(name))
+    write_greeting_to_file(name, outfile)
 
-@main.command()
-@click.option('--name', prompt='Please enter your name', help='The name to greet.')
-def hello(name):
-    """CLI entry point."""
-    click.echo(_hello(name))
-
-def _hello(name: str) -> str:
+def hello(name: str) -> str:
     """Function that returns a greeting string."""
     return f"Hello {name}"
+
+def write_greeting_to_file(name: str, file_path: str = "hello.txt") -> None:
+    """
+    Writes the greeting string to a specified text file.
+
+    Args:
+        name (str): The name to include in the greeting.
+        file_path (str, optional): The path to the text file. Defaults to 'hello.txt'.
+
+    Returns:
+        None
+    """
+    greeting = hello(name)
+    with open(file_path, 'w') as file:
+        file.write(greeting)
 
 schemas = OmegaConf.load(os.path.join(os.path.dirname(__file__), "../cabs/hello.yml"))
 
@@ -28,4 +40,3 @@ def clickify(name, schema_name=None):
         main.command(name, help=schemas.cabs.get(f'afronomy.{schema_name}').info,no_args_is_help=True)(
                 clickify_parameters(schemas.cabs.get(f'afronomy.{schema_name}'))(func)
         )
-
